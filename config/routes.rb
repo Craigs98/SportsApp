088acec6e22d6d2e8054e471692e25e3b33fad3e
Craigs98 <craigs98@outlook.com>
2021-04-14 22:38:37 +0100

    Administration created, updated posts page and live twitter feed on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :posts
-  devise_for :users
-  get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    devise_for :users
+    get 'home/index'
 
-   root to: "home#index"
+    root to: "home#index"
 end
